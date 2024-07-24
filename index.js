8b1347b5cab948f38026478b444186f5d5767971
cherrycharan238 <cherryiiit1234@gmail.com>
2024-07-24 11:05:19 +0530

    working in local repo

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // MongoDB connection
@@ -21,14 +21,17 @@ mongoose.connect('mongodb://localhost:27017/mytestdb').then(() => {
 const User = require('./models/User');
 
 app.get('/', async (req, res) => {
-  const users = await User.find();
-  res.render('index', { users });
+  res.render('index');
 });
 
 app.post('/add-user', async (req, res) => {
+  console.log(req.body);
   const newUser = new User(req.body);
   await newUser.save();
-  res.redirect('/');
+  const users = await User.find();
+  res.render('index' , {
+    users : users
+  });
 });
 
 const PORT = process.env.PORT || 3000;
